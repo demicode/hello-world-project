1bf5dac2e143f82b02309684fd36e0c4fa23d6c4
Mikael Degerfalt <micke@demicode.se>
2014-04-19 15:51:36 +0200

    Added two extra display modes.

--- atari8bit/hello-atari8.asm
+++ atari8bit/hello-atari8.asm
@@ -1,21 +1,14 @@
 	opt h+
-	org $4000 ;Start of code 
-start
-	; custom nmi
+	org $2000 ;Start of code
 
-	lda	#0		; Disable all interrupts
-	sta	$d40e
-
-	lda #0
+start:
+	lda	#0
+	sta	$d40e	; Disable all interrupts
 	sta	$d400	; disable all screen dma
 
-
-	lda	#<dl_irq
-	sta	$200
-	lda	#>dl_irq
-	sta	$201
-
-
+	; Install VBL interrupt handler
+	; at $222, which will disable all
+	; OS functionality
 	lda	#<vbl_irq
 	sta	$222
 	lda	#>vbl_irq
@@ -27,14 +20,71 @@ start
 	lda	#>disp_list
 	sta	$d403
 
-
 	; save pointer to video ram in zero page 
-	lda	#<end
+	lda	#<screen_mem
 	sta	$b0
 	sta	screen_ptr
-	lda	#>end
+	lda	#>screen_mem
 	sta	$b1
 	sta	screen_ptr+1
+
+
+	lda	#10
+	jsr	copy_1bit_gfx
+
+	lda	#<(screen_mem+10*7)
+	sta	$b0
+	lda	#>(screen_mem+10*7)
+	sta	$b1
+
+	lda	#20
+	jsr	copy_1bit_gfx
+
+	lda	#<(screen_mem+10*7 + 20*7 )
+	sta	$b0
+	lda	#>(screen_mem+10*7 + 20*7 )
+	sta	$b1
+
+	lda	#40
+	jsr	copy_1bit_gfx
+
+
+	lda	#$0f
+	sta	$d016	; set pf0 color to white
+	sta	$d017	; set pf1 color to white
+
+	lda	#$0
+	sta	$d018	; set pf2 color to black
+
+
+	lda	#$c0	; enable DLI and VBI
+	sta	$d40e
+
+	; 32 = enable DMA fetch, 16 = single line, 2 = normal pf
+	; 1 = narrow pf -> (1+2) = wide pf
+	lda	#32+16+2
+	sta $d400		; Re-enable screen DMA
+
+loop
+    jmp loop 		; loop forever
+
+;------------
+vbl_irq:
+    ; OS pushes the registers before jumping here, better restore them.
+    lda	#0
+    sta	$d01b	; clear GTIA bits of PRIOR reg
+	pla
+	tay
+	pla
+	tax
+	pla
+	rti
+
+copy_1bit_gfx:
+	; params: a - width of scanline in bytes
+	; 		$b0-$b1 dest address
+
+	sta	$b4
 
 	; setup ptr to graphics in zero page
 	lda #<hello_world_1col
@@ -44,18 +94,18 @@ start
 
 	ldx	#6	; six rows
 	ldy	#0
-byte_loop:
+?byte_loop:
 	lda	($b2),y
 	sta	($b0),y
 	iny
 	cpy #6
-	bmi	byte_loop
+	bmi	?byte_loop
 
 	dex
 	beq copy_done
 	ldy	#0
 
-	lda	#10
+	lda	$b4
 
 	; add line width to dest ptr
 	clc
@@ -74,59 +124,39 @@ byte_loop:
 	adc	$b3
 	sta	$b3
 
-	jmp byte_loop
-
+	jmp ?byte_loop
+;---
 copy_done:
-
-
-	lda	#$c0	; enable DLI and VBI
-	sta	$d40e
-
-	; 32 = enable DMA fetch, 16 = single line, 2 = normal pf
-	; 1 = narrow pf -> (1+2) = wide pf
-	lda	#32+16+2
-	sta $d400		; Re-enable screen DMA
-
-loop
-    jmp loop 
+	rts
 
 ;------------
-vbl_irq:
-	lda	#$92
-	sta $d01a 	; Change background color
-
-    ; OS stores the registers before jumping here, better restore them.
-	pla
-	tay
-	pla
-	tax
-	pla
-	rti
-
-;------------
-dl_irq:
-	pha
-	; setting background color here to produce rasterbars?
-
-	lda	#$f6
-	sta $d40a ; wsync (wait for horizontal sync)
-	sta $d01a 	;Change background color 
-
-
-	pla
-	rti
-;------------
-	org $4200
+	org $2200
 disp_list:
-	.db $70,$70
-	.db 9 + $40
+	.db $70,$70, $70
+	.db $70,$70, $70
+	.db $9 + $40		
 screen_ptr
 	.dw	start
-	.db 9
-	.db 9 + 128
-	.db 9
-	.db 9
-	.db 9
+	.db $9
+	.db $9
+	.db $9
+	.db $9
+	.db $9
+	.db $9
+	.db $b
+	.db $b
+	.db $b
+	.db $b
+	.db $b
+	.db $b
+	.db $b
+	.db $f
+	.db $f
+	.db $f
+	.db $f
+	.db $f
+	.db $f
+	.db $f
 	.db	$41
 	.dw	disp_list
 
@@ -140,8 +170,6 @@ hello_world_1col:
     .db    %01000101,%00000101,%01001001,%01010100,%10100001,%01001000
     .db    %01000100,%11110101,%00110000,%10100011,%00100001,%00111010
 
-end:
-
+screen_mem:
 
     run start ;Define run address must be last
-
