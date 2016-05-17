fe1074b6c23ac1083e81bc22512fd5a99975e9b9
Mikael Degerfalt <micke@demicode.se>
2016-05-17 21:28:32 +0200

    GB code fixed to work as intended
    I was trying to write to tile ram while the display was on. Now I wait for vertical blank, disable the LCD while writing to the video ram.
    
    Also I converted all tabs to spaces, mostly to make it a less annoying task to write the blog post. Editing text in a browser where you want to use tabs is really vexing.

--- gameboy/hello_gb.s
+++ gameboy/hello_gb.s
@@ -4,7 +4,7 @@
 ; Orignal Gameboy version.
 ;
 
-; WLA DX setup. 
+; WLA DX setup.
 .MEMORYMAP
   SLOTSIZE $4000
   DEFAULTSLOT 0
@@ -22,158 +22,114 @@
 
 ; All jump vectors just jump to main code.
 .bank 0
-.orga $00 
-	ret
+.orga $00
+    ret
 
 ; Interrupt vectors.
 .orga $40
 vbl:
-	reti
+    reti
 
-.org $48	
+.org $48
 lcd:
-	reti
+    reti
 
 .orga $50
 timer:
-	reti
+    reti
 
 .orga $58
 serial:
-	reti
+    reti
 
 .orga $60
 joypad:
-	reti
+    reti
 
 .orga $100
-	.db 0
-	jp main
+    .db 0
+    jp main
 
-	; $0104-$0133 
-	; (Nintendo logo - do _not_ modify the logo data here or the GB will not run the program)
+    ; $0104-$0133
+    ; (Nintendo logo - do _not_ modify the logo data here or the GB will not run the program)
 .orga $104
-	.db	$ce,$ed,$66,$66,$cc,$0d,$00,$0b,$03,$73,$00,$83,$00,$0c,$00,$0d
-	.db	$00,$08,$11,$1f,$88,$89,$00,$0e,$dc,$cc,$6e,$e6,$dd,$dd,$d9,$99
-	.db	$bb,$bb,$67,$63,$6e,$0e,$ec,$cc,$dd,$dc,$99,$9f,$bb,$b9,$33,$3e
+    .db $ce,$ed,$66,$66,$cc,$0d,$00,$0b,$03,$73,$00,$83,$00,$0c,$00,$0d
+    .db $00,$08,$11,$1f,$88,$89,$00,$0e,$dc,$cc,$6e,$e6,$dd,$dd,$d9,$99
+    .db $bb,$bb,$67,$63,$6e,$0e,$ec,$cc,$dd,$dc,$99,$9f,$bb,$b9,$33,$3e
 
 ; This block tells wla dx to generate a GB rom with the expected values
 ; in the header.
 
 .NAME "HELLOWORLD " ; must be 11 bytes.
 .COMPUTEGBCHECKSUM
-.CARTRIDGETYPE $1
+.CARTRIDGETYPE $0
 .LICENSEECODENEW "1A"
 .ROMDMG
 .COMPUTEGBCOMPLEMENTCHECK
 
+.orga $14a
+    .db $01     ; destination country, 00 is japan, 01 is other
+.orga   $14c
+    .db $00     ; mask ROM version
 .orga $150
 main:
-	di
+    di
 
-	; ldh is a special opcode for gameboy.
-	; ldh (x),a essentially writes content of a to address $ff00 + x
-	; by coincidence the gameboys memory mapped i/o is in address range $ff00 -> $ffff
+    ; ldh is a special opcode for gameboy.
+    ; ldh (x),a essentially writes content of a to address $ff00 + x
+    ; by coincidence the gameboys memory mapped i/o is in address range $ff00 -> $ffff
 
-	ld	a,0
-	ldh	($ff),a		
-	sub	a
-	ldh	($41),a
-	ldh	($42),a
-	ldh	($43),a
+    ; Wait for vblank period before disabling lcd controller
+-:  ldh a,($44)
+    sub 144
+    jp  c,-
 
+    xor a           ; clear register a
+    ldh ($40),a     ; disable LCD and stuff
 
-	ld sp,$d900  	; Setup stackpointer.
+    ; clear screen memory
+    ld  hl,$9bff
+-:  xor a
+    ldd (hl),a
+    ld  a,h
+    sub $97
+    jp nz,-
 
-	ld hl,$8000		; Load address to tile ram
-	ld de,gfx		; Load address to graphics data.
-	ld b,8*7		; seven tiles of 8 bytes of data.
-					; Not that tile data is actually 16 bytes, but
-					; we duplicate the data on two bilplanes in loop.
+    ld hl,$8000     ; Load address to tile ram
+    ld de,gfx       ; Load address to graphics data.
+    ld b,8*7        ; seven tiles of 8 bytes of data.
+                    ; Not that tile data is actually 16 bytes, but
+                    ; we duplicate the data on two bilplanes in loop.
 -:
-	ld a,(de)		; Get graphics data.
-	ldi (hl),a		; load and increase
-	ldi (hl),a		; same data on both bitplanes
-	inc de 			; increase source
-	dec b			; decrease register b
-	jp nz,-			; loop if result not zero
+    ld a,(de)       ; Get graphics data.
+    ldi (hl),a      ; load and increase
+    ldi (hl),a      ; same data on both bitplanes
+    inc de          ; increase source
+    dec b           ; decrease register b
+    jp nz,-         ; loop if result not zero
 
 
-	ld hl,$9800		; Load address to tilemap
-	ld a,1			; first tile to set (tile 0 is empty)
-	ld b,6			; Hello world consists of 6 tiles
+    ld hl,$9800     ; Load address to tilemap
+    ld a,1          ; first tile to set (tile 0 is empty)
+    ld b,6          ; Hello world consists of 6 tiles
 -:
-	ldi (hl),a		; Write tile number to tilemap
-	inc a			; next tile number
-	dec b			; decrease counter
-	jp nz,-			; repeat until b is 0.
+    ldi (hl),a      ; Write tile number to tilemap
+    inc a           ; next tile number
+    dec b           ; decrease counter
+    jp nz,-         ; repeat until b is 0.
 
+    ld  a,%10010001 ; LCD Controller = On  BG = On  Sprites = Off
+    ldh ($40),a
+-:
 
+    halt
+    jp -            ; endless loop
 
-
-	ld	a,%10010001	; LCD Controller = On  BG = On  Sprites = Off
-	ldh	($40),a
-	
--:	
-
-
-	halt
-	jp -			; endless loop
-
-gfx:  
-	.db     $00,$00,$00,$00,$00,$00,$00,$00
-
-	.db     %00000000
-	.db     %01000100
-	.db     %01000100
-	.db     %01000101
-	.db     %01111101
-	.db     %01000101
-	.db     %01000100
-	.db     %00000000
-
-	.db     %00000000
-	.db     %00000101
-	.db     %11100101
-	.db     %00010101
-	.db     %11110101
-	.db     %00000101
-	.db     %11110101
-	.db     %00000000
-
-	.db     %00000000
-	.db     %00000001
-	.db     %00110001
-	.db     %01001001
-	.db     %01001001
-	.db     %01001001
-	.db     %00110000
-	.db     %00000000
-
-	.db     %00000000
-	.db     %00010000
-	.db     %00010011
-	.db     %00010100
-	.db     %01010100
-	.db     %01010100
-	.db     %10100011
-	.db     %00000000
-
-	.db     %00000000
-	.db     %00000001
-	.db     %00011001
-	.db     %10100101
-	.db     %10100001
-	.db     %10100001
-	.db     %00100001
-	.db     %00000000
-
-	.db     %00000000
-	.db     %00001010
-	.db     %00111010
-	.db     %01001010
-	.db     %01001010
-	.db     %01001000
-	.db     %00111010
-	.db     %00000000
-
+gfx:
+    .db     $00,$00,$00,$00,$00,$00,$00,$00
+    .db     $00,$44,$44,$45,$7d,$45,$44,$00
+    .db     $00,$05,$e5,$15,$f5,$05,$f5,$00
+    .db     $00,$01,$31,$49,$49,$49,$30,$00
+    .db     $00,$10,$13,$14,$54,$54,$a3,$00
+    .db     $00,$01,$19,$a5,$a1,$a1,$21,$00
+    .db     $00,$0a,$3a,$4a,$4a,$48,$3a,$00
