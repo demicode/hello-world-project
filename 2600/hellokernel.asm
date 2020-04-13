22735fbbed1eaec8119bf42e4f3668a88cb656bb
Mikael Degerfält <micke@demicode.se>
2020-04-13 13:59:37 +0200

    Working 2600 code

--- 2600/hellokernel.asm
+++ 2600/hellokernel.asm
@@ -1,17 +1,21 @@
 ; Hello world for atari 2600
 
-
 VSYNC   equ 0
 VBLANK  equ 1
 WSYNC   equ 2
+COLUPF  equ 8
 COLUBK  equ 9
-
+PF0     equ $d
+PF1     equ $e
+PF2     equ $f
 
     .processor 6502
     seg
     org $f000
 
 start
+    cld
+    sei
     lda     #0
     tax
     txs
@@ -25,7 +29,6 @@ start
 
 
 kernel_loop: SUBROUTINE
-    sei
     lda     #0
     sta     VBLANK
     lda     #2
@@ -55,51 +58,50 @@ DATA_ADDR set datas
     .repeat 6     ; 6 rows
 
     ldy     #8
-apa set .
-    sta     8   ; background color
+row_block set .
+    sta     COLUPF   ; playfield color
 
     ldx     DATA_ADDR
-    stx     $0d
+    stx     PF0
     ldx     DATA_ADDR+1
-    stx     $0e
+    stx     PF1
     ldx     DATA_ADDR+2
-    stx     $0f
+    stx     PF2
 
     nop
     nop
 
     ldx     DATA_ADDR+3
-    stx     $0d
+    stx     PF0
     ldx     DATA_ADDR+4
-    stx     $0e
+    stx     PF1
     ldx     DATA_ADDR+5
-    stx     $0f
+    stx     PF2
 
     clc
-    adc     #2
+    adc     #2      ; Add 2 to accumulator, used for playfield colour
 
     sta     WSYNC
 
     dey
-    bne     apa
+    bne     row_block
 
 DATA_ADDR set DATA_ADDR + 6
 
     .repend
 
-
-    ldy #0
-    sty $d
-    sty $e
-    sty $f
-    sty 9
-    sty 8
+    ; Clear playfield data and set colours to black
+    ldy     #0
+    sty     PF0
+    sty     PF1
+    sty     PF2
+    sty     COLUBK
+    sty     COLUPF
 
     ldy     #0
     ldx     #165-36
 .display
     sta     WSYNC
-
     dex
     bne     .display
 
@@ -112,7 +114,7 @@ DATA_ADDR set DATA_ADDR + 6
     dex
     bne     .overscan
 
-    jmp kernel_loop
+    jmp     kernel_loop
 
 datas:
     dc.b    %10100000,%00000101,%01000000,%01000000,%00000000,%01010001
@@ -122,8 +124,6 @@ datas:
     dc.b    %10100000,%01000101,%01001010,%01010000,%10101000,%00010101
     dc.b    %10100000,%00110101,%10000100,%00100000,%01001000,%01011001
     dc.b    %00000000,%00000000,%00000000,%00000000,%00000000,%00000000
-
-
 
     org $fffc
     .word start
