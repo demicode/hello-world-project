995958d88c19a4f657680d36c21c120a7d5e3875
Mikael Degerfält <mikael@demicode.se>
2023-07-03 21:19:23 +0100

    Python3 support for GBA header script

--- gba/fix_gba_header.py
+++ gba/fix_gba_header.py
@@ -2,19 +2,19 @@
 
 import sys, struct
 
-f = open( sys.argv[1], 'r+b' )
+f = open(sys.argv[1], 'r+b')
 
 f.seek( 0xa0, 0 ) # skip header
 romdata = f.read( 0xbd-0xa0 )
 
 cnt = len(romdata)
-checksum = sum( struct.unpack( "%sb"%(cnt), romdata) ) & 0xff
-compliment = (-(0x19+checksum)) &0xff
+checksum = sum(struct.unpack("%sb"%(cnt), romdata)) & 0xff
+compliment = (-(0x19+checksum)) & 0xff
 
-f.seek( 0xbd, 0 ) # seek to compliment position in rom
+f.seek(0xbd, 0) # seek to compliment position in rom
 
 r = struct.pack("B",compliment)
 
 f.write( r ) # write checksum
 f.close()
-print "ROM compliment %s witten to file %s"%( r, sys.argv[1] )
+print(f"ROM compliment {r} witten to file {sys.argv[1]}")
